e989f6dab38ff44ae7d5cd375ff418e0b331b8a1
Nicolas Cruz <nicocruzur04@gmail.com>
2025-07-03 17:18:00 -0500

    Clase servicio usuario

--- src/servicio/UsuarioService.java
+++ src/servicio/UsuarioService.java
@@ -1,5 +1,40 @@
 //Permite agendar, listar, cancelar citas.
 package servicio;
 
+import modelo.Medico;
+import modelo.Paciente;
+import modelo.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioService {
+    private List<Usuario> usuarios;
+
+    public UsuarioService() {
+        usuarios = new ArrayList<>();
+    }
+
+    public boolean registrarUsuario(Usuario usuario) {
+        for (Usuario u : usuarios){
+            if(u.getCorreo().equalsIgnoreCase(usuario.getCorreo())){
+                return false;
+            }
+        }
+        usuarios.add(usuario);
+        return true;
+    }
+
+    public Usuario iniciarSesion(String correo, String contrasena) {
+        for (Usuario u : usuarios) {
+            if (u.getCorreo().equalsIgnoreCase(correo) && u.getContrasena().equals(contrasena)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
 }
